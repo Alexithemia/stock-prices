df2683b9a7afe843f7f8b0ae05c1f69dead17027
Tyler <alexithemia@gmail.com>
2019-01-11 10:49:24 -1000

    Setup to check for profit from random prices

---
+++ stock-profit.js
@@ -0,0 +1,41 @@
+document.getElementById('random').addEventListener('click', randomStocks);
+document.getElementById('check').addEventListener('click', checkIfProfit)
+let prices = [];
+function randomStocks() {
+  prices.length = 0;
+  let stocks = document.getElementById('stocks');
+  stocks.innerHTML = '';
+  for (let i = 0; i < 10; i++) {
+    prices.push(Math.floor(Math.random() * 60) + 30)
+    let stockPrice = document.createElement('div');
+    stockPrice.className = 'stockPrice';
+    stockPrice.innerHTML = prices[i];
+    stocks.appendChild(stockPrice);
+  }
+}
+
+function checkIfProfit() {
+  let stocks = document.getElementById('stocks');
+  let hasProfit = document.createElement('div');
+  hasProfit.className = 'hasProfit';
+  stocks.appendChild(hasProfit);
+  let profit = 0;
+  let buy = 0;
+  let sell = 0;
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i; j < prices.length; j++) {
+      if ((prices[i] - prices[j]) > profit) {
+        profit = prices[i] - prices[j];
+        buy = prices[i];
+        sell = prices[j];
+      }
+    }
+  }
+  if (profit > 0) {
+    hasProfit.innerHTML = 'buy at ' + buy + ' sell at ' + sell + " for a profit of " + profit;
+  } else {
+    hasProfit.innerHTML = 'no profit'
+  }
+}
+
+
